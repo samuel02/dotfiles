6b5b988ab2a5c1fa905343f3ebcea93087aed3e8
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-01-24 13:08:56 +0100

    Add GOPATH to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,4 +2,4 @@ export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 export GOPATH=~/go
-#export PATH="$GOPATH/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
