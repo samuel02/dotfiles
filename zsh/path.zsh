fa7493bf3f19ea87fc54f62d22ba21684c26e9e8
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-03-09 14:41:57 +0100

    Fix python setup

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -3,3 +3,4 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export GOPATH=~/go
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$PATH:/usr/local/opt/fzf/bin"
+export PATH="/usr/local/opt/python@2/bin:$PATH"
