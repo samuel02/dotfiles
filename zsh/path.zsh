774aaebbb6cf59a7bb04f2c34f8a0376860ad955
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-04-05 15:05:58 +0200

    Add git safe path trick

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,8 @@
-export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export GOPATH=~/go
+
+export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$PATH:/usr/local/opt/fzf/bin"
 export PATH="/usr/local/opt/python@2/bin:$PATH"
+export PATH=".git/safe/../../bin:$PATH"
