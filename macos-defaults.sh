4bc211924aceed7f3bd440f6a3ac1eeaca828306
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-09-19 11:48:42 +0200

    Disable press-and-hold

--- macos-defaults.sh
+++ macos-defaults.sh
@@ -1,3 +1,6 @@
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 # Always open everything in Finder's list view.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
