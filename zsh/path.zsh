1553b62a42364cddc7c6096f3c42ccdf19cdd127
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-10-31 16:33:55 +0100

    Add python user packages to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,11 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export GOPATH=~/go
+export PYTHON_USER_BASE_PATH=$(python3 -m site --user-base)
 
 export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$PATH:/usr/local/opt/fzf/bin"
 export PATH="/usr/local/opt/python@2/bin:$PATH"
+export PATH="/usr/local/opt/python@3/bin:$PATH"
+export PATH="$PYTHON_USER_BASE_PATH/bin:$PATH"
 export PATH=".git/safe/../../bin:$PATH"
