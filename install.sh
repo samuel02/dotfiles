759373ccc967e29b2348837cac31c95a2583ede0
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-11-02 11:11:27 +0100

    Clean install.sh

--- install.sh
+++ install.sh
@@ -1,69 +1,65 @@
 #!/bin/sh
 
+indent() { sed 's/^/  /'; }
+info() { echo "› \033[0;32m$1\033[0m"; }
+
 DOTFILES="$HOME/.dotfiles"
 
 # Install homebrew unless already installed
-if test ! "$(which brew)"
-then
-    echo "› installing homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if test ! "$(command -v brew)"; then
+  echo "› installing homebrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update homewbrew formulas
-echo "› brew update"
-brew update
+info "brew update"
+brew update | indent
 
 # Install all formulas in Brewfile
-echo "› brew bundle"
-brew bundle
+info "brew bundle"
+brew bundle | indent
 
 # Set shell
 # If this user's login shell is not already "zsh", attempt to switch.
-TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-  echo "› chsh -s /usr/local/bin/zsh"
-  chsh -s $(grep /zsh$ /etc/shells | tail -1)
+test_current_shell=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$test_current_shell" != "zsh" ]; then
+  info "set shell to zsh"
+  chsh -s "$(grep /zsh$ /etc/shells | tail -1)"
 fi
 
-# Install python packages
-echo "› installing python packages"
-pip3 install gcalcli pygments --pre
-curl https://raw.githubusercontent.com/gthank/solarized-dark-pygments/master/solarized256.py \
-  --output /usr/local/lib/python3.7/site-packages/pygments/styles/solarized256.py
-
 # Setup dotfiles
-echo "› setup dotfiles"
+info "setup dotfiles"
 RCRC="$DOTFILES/rcrc" rcup
 
 # Copy example files
-echo "› add local config files"
+info "add local config files"
 for file in "$HOME"/.dotfiles/*.example; do
   basename="$(basename "$file")"
   src=$file
   dest="$HOME/.${basename%.*}"
 
-  cp -v -n "$src" "$dest"
+  cp -v -n "$src" "$dest" | indent
 done
 
 # Install vim plugins
-echo "› install vim-plug"
+info "install vim-plug"
 if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
   vim -E -s +PlugUpgrade +qa
 else
   curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | indent
 fi
 
-echo "› install vim plugins"
-vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
+info "install vim plugins"
+vim +PlugUpdate +PlugClean! +qa
 
 # Setup bat(https://github.com/sharkdp/bat)
-echo "› install bat"
-BAT_CONFIG_DIR="$(bat cache --config-dir)"
-
-mkdir -p "$BAT_CONFIG_DIR/themes"
-cd "$BAT_CONFIG_DIR/themes"
-git clone https://github.com/drewish/Solarized-1
-
-# Update the binary cache
-bat cache --init
+bat_config_dir="$(bat cache --config-dir)"
+bat_themes_dir="$bat_config_dir/themes"
+bat_solarized_theme="$bat_themes_dir/Solarized-1"
+if [ ! -d "$bat_solarized_theme" ]; then
+  info "setup bat"
+  mkdir -p "$bat_themes_dir"
+  git clone "https://github.com/drewish/Solarized-1" "$bat_solarized_theme"
+  bat cache --init
+fi
